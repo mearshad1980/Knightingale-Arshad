483c900efc121e2024e44e853bff126f4fcac70d
mearshad1980 <87507516+mearshad1980@users.noreply.github.com>
2021-07-16 07:13:21 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,21 @@
+package hit.day3;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // Data declaration....
+		
+		int i=100;
+        int j=200;
+        
+       // Processing of data...
+        
+        int sum= i+j;
+        
+       // Output
+        
+        System.out.println("The result is....:"+sum);
+	}
+
+}
